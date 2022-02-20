84f61718dde9c0e0e8769342cc3c5b0b51d5608c
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-02-20 19:01:45 +0300

    Add html parsing example

--- views/parseallcolleges/index.php
+++ views/parseallcolleges/index.php
@@ -1,1 +1,12 @@
-<?= phpversion() ?>
+<?php
+$url = 'http://acid3.acidtests.org';
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$html = curl_exec($handle);
+libxml_use_internal_errors(true); // Prevent HTML errors from displaying
+$doc = new DOMDocument();
+$doc->loadHTML($html);
+$link = $doc->getElementsByTagName('a')->item(0);
+echo 'Link text: ' . $link->nodeValue;
+echo '<br />';
+echo 'Link URL: ' . $link->getAttribute('href');
