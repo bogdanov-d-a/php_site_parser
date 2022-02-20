4f9e0ea4d9a4d629eb0e3e26d8d633187b8c2f29
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-02-20 19:14:54 +0300

    Move logic to controller

--- controllers/ParseallcollegesController.php
+++ controllers/ParseallcollegesController.php
@@ -8,6 +8,18 @@ class ParseallcollegesController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $url = 'http://acid3.acidtests.org';
+        $handle = curl_init($url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $html = curl_exec($handle);
+        libxml_use_internal_errors(true); // Prevent HTML errors from displaying
+        $doc = new \DOMDocument();
+        $doc->loadHTML($html);
+        $link = $doc->getElementsByTagName('a')->item(0);
+
+        return $this->render('index', [
+            'linkText' => $link->nodeValue,
+            'linkURL' => $link->getAttribute('href'),
+        ]);
     }
 }
--- views/parseallcolleges/index.php
+++ views/parseallcolleges/index.php
@@ -1,12 +1,3 @@
-<?php
-$url = 'http://acid3.acidtests.org';
-$handle = curl_init($url);
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-$html = curl_exec($handle);
-libxml_use_internal_errors(true); // Prevent HTML errors from displaying
-$doc = new DOMDocument();
-$doc->loadHTML($html);
-$link = $doc->getElementsByTagName('a')->item(0);
-echo 'Link text: ' . $link->nodeValue;
-echo '<br />';
-echo 'Link URL: ' . $link->getAttribute('href');
+<?= 'Link text: ' . $linkText ?>
+<br/>
+<?= 'Link URL: ' . $linkURL ?>
