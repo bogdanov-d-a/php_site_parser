189445c640a364eb46ef293b7b52c7c704e2f743
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-02-22 18:42:09 +0300

    Generate Allcolleges CRUD

---
+++ views/allcolleges/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Allcolleges */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="allcolleges-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'imgurl')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'name')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'city')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'state')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/allcolleges/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\AllcollegesSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Allcolleges';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="allcolleges-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Allcolleges', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'imgurl:ntext',
+            'name:ntext',
+            'city:ntext',
+            'state:ntext',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Allcolleges $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
