07bef5aff5ee5d6e16fc264d37cd7a8dbbda7ce4
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-02-23 16:55:36 +0300

    Parse colleges from DB

--- controllers/ParseonecollegeController.php
+++ controllers/ParseonecollegeController.php
@@ -4,15 +4,28 @@ namespace app\controllers;
 
 use yii\web\Controller;
 use app\controllers\OnecollegeDataParser;
+use app\models\Onecollege;
 use yii\BaseYii;
 
 class ParseonecollegeController extends Controller
 {
     public function actionIndex()
     {
-        $parseResult = OnecollegeDataParser::parse('https://www.princetonreview.com/college/harvard-college-1022984?ceid=cp-1022984');
-        BaseYii::debug('ParseonecollegeController::actionIndex');
-        BaseYii::debug($parseResult);
+        foreach (Onecollege::find()->where('`needupd`')->all() as $onecollege)
+        {
+            $url = $onecollege->srcurl;
+            BaseYii::debug('ParseonecollegeController::actionIndex parse ' . $url);
+            $parseResult = OnecollegeDataParser::parse($url);
+            sleep(1);  // reduce server request rate
+
+            $onecollege->needupd = false;
+            $onecollege->address = $parseResult['address'];
+            $onecollege->phone = $parseResult['phone'];
+            $onecollege->siteurl = $parseResult['siteurl'];
+            $onecollege->name = $parseResult['name'];
+            $onecollege->save();
+        }
+
         return $this->render('index');
     }
 }
