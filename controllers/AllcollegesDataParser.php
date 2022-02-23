a7b39ed0ee4e0c8b5524313c3a0f4ca65015a98d
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-02-23 22:40:59 +0300

    Disable $pageCount limiter

--- controllers/AllcollegesDataParser.php
+++ controllers/AllcollegesDataParser.php
@@ -238,7 +238,7 @@ class AllcollegesDataParser
         $pageCount = $parsePage1Result['pageCount'];
 
         // for debugging purposes
-        $pageCount = 2;
+        //$pageCount = 2;
 
         for ($pageNumber = 2; $pageNumber <= $pageCount; $pageNumber++)
         {
